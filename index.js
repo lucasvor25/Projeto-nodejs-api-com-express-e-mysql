db937021786054d0acbf080493590b25e83c5830
Your Name <you@example.com>
2021-11-07 08:10:01 -0300

    criando customexpress e alguns imports

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const express=require('express');
-const app = express();
+const customExpress = require('./config/customExpress');
 
-app.listen(3000, () => console.log('servidor rodando'));
-app.get('/atendimentos', (req, res) => res.send('Voce está no atendimento'));
+const app = customExpress();
+app.listen(3000, () => console.log('servidor'));
