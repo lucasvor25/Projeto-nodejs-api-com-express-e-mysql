e1909b32dc6f4edc17b9a076e26a9f4e84e35d6e
Your Name <you@example.com>
2021-11-07 08:21:19 -0300

    criando app.post

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,6 @@
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Voce está no atendimento'));
+
+    app.post('/atendimentos', (req,res) => res.send('Voce esta na rota de atendimentos'));
+
 }
