c33203e360ca95a5e21b71f4ba659ed1bed3d2f4
Your Name <you@example.com>
2021-11-08 22:28:47 -0300

    colocando data de criacao na tabela

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,11 +2,11 @@ const Atendimento = require('../models/atendimentos')
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Voce está no atendimento'));
 
-    app.post('/atendimentos', (req,res) =>{
-   const atendimento = req.body;
+    app.post('/atendimentos', (req, res) => {
+        const atendimento = req.body;
 
-   Atendimento.adiciona(atendimento)
-     res.send('Voce esta na rota de atendimentos')
+        Atendimento.adiciona(atendimento)
+        res.send('Voce esta na rota de atendimentos')
 
-})
+    })
 }
