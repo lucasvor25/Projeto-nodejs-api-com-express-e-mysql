88c9ef31e54b1f4ceef3845700a03314c4a41e06
Your Name <you@example.com>
2021-11-08 22:06:07 -0300

    adicionada a tabela dentro do mysql usando o postman

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,8 +1,11 @@
+const Atendimento = require('../models/atendimentos')
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Voce está no atendimento'));
 
     app.post('/atendimentos', (req,res) =>{
-    console.log(req.body)
+   const atendimento = req.body;
+
+   Atendimento.adiciona(atendimento)
      res.send('Voce esta na rota de atendimentos')
 
 })
